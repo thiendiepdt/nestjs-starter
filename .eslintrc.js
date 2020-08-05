6dab07d40f5555e9c75801df23adfbb8d56d8393
thiendiepdt <thiendiepdt@gmail.com>
2020-08-05 15:19:17 +0700

    Edit eslint, prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: 'tsconfig.build.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
